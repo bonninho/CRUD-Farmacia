2ce09c6da484334c88be5fd23b369dc171db7973
Bonnie <biankabonete@gmail.com>
2024-07-22 09:32:49 -0300

    inicializando o projeto Farmacia

---
+++ src/main/java/com/generation/lojadegames/PgFarmaciaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.lojadegames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgFarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgFarmaciaApplication.class, args);
+	}
+
+}
